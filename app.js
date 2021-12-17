c13d73421be673bf8da67e7cbf17b9ca4f612139
Griffin Johnson <cougargriff@gmail.com>
2021-12-16 17:28:26 -0800

    added cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes/setup.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 routes.init(app);
+app.use(cors());
 app.listen(PORT);
 console.log(`App listening on port ${PORT}`);
